4743d697dbbe45b0592c543d3d21920df1199f1c
Sapphire0912 <kotori228520@gmail.com>
2020-09-24 19:03:47 +0800

    Test

--- Practice/DeepLearning/用Python進行深度學習的基礎理論實作/ch05_layer_naive.py
+++ Practice/DeepLearning/用Python進行深度學習的基礎理論實作/ch05_layer_naive.py
@@ -99,3 +99,54 @@ dy = relu.backward(y)
 # print(dy)
 
 # Sigmoid 層
+# class Sigmoid:
+#     def __init__(self):
+#         self.out = None
+    
+#     def forward(self, x):
+#         out = 1 / (1 + np.exp(-x))
+#         self.out = out
+#         return out
+    
+#     def backward(self, dout):
+#         dx = dout * (1.0 - self.out) * self.out
+#         return dx
+
+# Affine 層
+# class Affine:
+#     def __init__(self, W, b):
+#         self.W = W
+#         self.b = b
+#         self.x = None
+#         self.dw = None
+#         self.db = None
+    
+#     def forward(self, x):
+#         self.x = x
+#         out = np.dot(X, self.W) + self.b
+#         return out
+
+#     def backward(self, dout):
+#         dx = np.dot(dout, self.W.T)
+#         self.dW = np.dot(self.x.T, dout)
+#         self.db = np.sum(dout, axis = 0)
+#         return dx
+
+# Softmax-with-loss 層
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None 
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return loss
+    
+    def backward(self, dout = 1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
