d64bb91fe20ca60494f82f621d0ff040c43ac8bc
Sapphire0912 <kotori228520@gmail.com>
2020-09-29 10:45:09 +0800

    Project_example

---
+++ Project/教學/Ch01 Python 基礎知識/Tkinter 事件處理.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+# 事件類型的通用格式如下
+# <[modifier-]...type[-detail]>
+# 事件類型必須放置於中括號<>內, type 描述類型(例如 鍵盤按鍵, 滑鼠點擊), modifier 用於組合鍵定義(例如 Ctrl Alt) 
+# detail 用來明確定義是哪個鍵或按鈕事件(例如 1 滑鼠左鍵, 2 滑鼠中鍵, 3 滑鼠右鍵)
+# 例子: <Button-1> 按下滑鼠左鍵, <KeyPress-A> 按下鍵盤上的A鍵, <Control-Shift-KeyPress-A> 同時按下 Ctrl+shift+A 
+
