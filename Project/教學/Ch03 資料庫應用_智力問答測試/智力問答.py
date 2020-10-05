2b1b122dbc38ba1f12e97cd55c7f214917c30e1c
Sapphire0912 <kotori228520@gmail.com>
2020-10-05 19:41:59 +0800

    Project_example

---
+++ Project/教學/Ch03 資料庫應用_智力問答測試/智力問答.py
@@ -0,0 +1,1 @@
+# wait (loss datasets)
---
+++ Project/教學/Ch04 API應用_翻譯器材/小小翻譯器.py
@@ -0,0 +1,10 @@
+# coding = utf8
+# 把 Google 翻譯 寫成 API
+
+# urllib 介紹
+# urllib.request: 用來讀取和開啟 URL
+# urllib.error: 包含一些由 urllib.request 產生的錯誤, 可以使用 try 進行捕捉處理
+# urllib.parse: 包含一些解析 URL 的方法
+# urllib.robotparser: 用來解析 robots.txt, 另外提供一個單獨的 RobotFileParser 類別, 透過該類別提供的 can_fetch() 方法測試爬蟲是否可以下載一個頁面
+
+# 
