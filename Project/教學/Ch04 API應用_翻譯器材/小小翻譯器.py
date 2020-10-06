bc8cb871f745e880154ad5e0e3c6ec0f86611cb5
Sapphire0912 <kotori228520@gmail.com>
2020-10-06 10:05:37 +0800

    Project_example

--- Project/教學/Ch04 API應用_翻譯器材/小小翻譯器.py
+++ Project/教學/Ch04 API應用_翻譯器材/小小翻譯器.py
@@ -1,10 +1,42 @@
-# coding = utf8
-# 把 Google 翻譯 寫成 API
+from tkinter import *
+from urllib import request
+from urllib import parse
+import json
 
-# urllib 介紹
-# urllib.request: 用來讀取和開啟 URL
-# urllib.error: 包含一些由 urllib.request 產生的錯誤, 可以使用 try 進行捕捉處理
-# urllib.parse: 包含一些解析 URL 的方法
-# urllib.robotparser: 用來解析 robots.txt, 另外提供一個單獨的 RobotFileParser 類別, 透過該類別提供的 can_fetch() 方法測試爬蟲是否可以下載一個頁面
+win = Tk()
+win.title("MyTranslator")
+win.geometry("250x130")
 
-# 
+def translate_word(en_str):
+    url = 'https://translate.google.com.tw/'
+    re
+
+
+def trans_click(event):
+    pass
+
+def clr_click(event):
+    pass
+
+# 輸入文字 以及 翻譯結果
+Label(win, text = "輸入要翻譯的內容: ", width = 15).place(x = 1, y = 1)
+ip = Entry(win, width = 20)
+ip.place(x = 110, y = 1)
+Label(win, text = "翻譯的結果: ", width = 18).place(x = 1, y = 20)
+s = StringVar()
+s.set("Test")
+result = Entry(win, width = 20, textvariable = s)
+result.place(x = 110, y = 20)
+
+# 翻譯 和 清空 按鈕
+trans = Button(win, text = "翻譯", width = 8)
+trans.place(x = 40, y = 80)
+clr = Button(win, text = "清空", width = 8)
+clr.place(x = 110, y = 80)
+
+# 將按鈕綁定事件
+trans.bind("<Button-1>", trans_click)
+clr.bind("<Button-1>", clr_click)
+
+
+win.mainloop()
