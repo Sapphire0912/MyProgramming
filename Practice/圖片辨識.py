9c4a11737d52d93ec6f68ba50a9c5102fd229256
Sapphire0912 <kotori228520@gmail.com>
2020-10-15 22:48:24 +0800

    test

--- Practice/圖片辨識.py
+++ Practice/圖片辨識.py
@@ -8,6 +8,10 @@ import cv2
 # print(img.shape) # (667, 1000, 3)
 
 
+def matplot_show(img):
+    plt.imshow(img, cmap = 'gray')
+    plt.show()
+
 # 偵測全身
 path_fullbody = 'C:\\Users\\iris2\\AppData\\Local\\Programs\\Python\\Python37\\Lib\site-packages\\cv2\\data\\haarcascade_fullbody.xml'
 
@@ -19,9 +23,9 @@ gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) # 用灰階開啟圖片
 
 bodies = body_classifier.detectMultiScale(
     img,
-    scaleFactor = 1.1,
+    scaleFactor = 1.01,
     minNeighbors = 3,
-    minSize = (30, 30)
+    minSize = (150, 30)
 )
 
 for (x, y, w, h) in bodies:
@@ -29,7 +33,9 @@ for (x, y, w, h) in bodies:
 
 cv2.namedWindow('MyImg', cv2.WINDOW_NORMAL) # 讓視窗可以任意縮放大小
 cv2.imshow("MyImg", img) # 顯示圖片
+cv2.imwrite('result.jpg', img)
+# matplot_show(gray)
 
 # 按下任意建可關閉視窗
-cv2.waitKey(0) 
+cv2.waitKey(0)
 cv2.destroyAllWindows()
