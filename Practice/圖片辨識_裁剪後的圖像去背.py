91fa6188df6381c93a534c5d69ccd29e1fda573e
Sapphire0912 <kotori228520@gmail.com>
2020-10-20 11:05:48 +0800

    test

--- Practice/圖片辨識_裁剪後的圖像去背.py
+++ Practice/圖片辨識_裁剪後的圖像去背.py
@@ -18,18 +18,26 @@ def blur_st(img, size = (9, 9), SD = 0):
     return cv2.GaussianBlur(img, size, SD)
 
 # 2. 邊緣檢測
-def edge_detection(img, ddepth = cv2.CV_64F, ksize = -1):
-    gradX = cv2.Sobel(img, ddepth = ddepth, dx = 1, dy = 0, ksize = ksize)
-    gradY = cv2.Sobel(img, ddepth = ddepth, dx = 0, dy = 1, ksize = ksize)
-    gradient = cv2.subtract(gradX, gradY)
-    gradient = cv2.convertScaleAbs(gradient)
-    return gradient
+def edge_detection(img, thres_min = 20, thres_max = 50):
+    # gradX = cv2.Sobel(img, ddepth = ddepth, dx = 1, dy = 0, ksize = ksize)
+    # gradY = cv2.Sobel(img, ddepth = ddepth, dx = 0, dy = 1, ksize = ksize)
+    # gradient = cv2.subtract(gradX, gradY)
+    # gradient = cv2.convertScaleAbs(gradient)
+    # return gradient
+
+    canny_ = cv2.Canny(img, thres_min, thres_max)
+    return canny_
+
+# 3. 消除雜訊後轉成二值圖
+def threshold(img):
+    pass
+
 
 # 4. 描繪輪廓
 def draw_contour(img, ori_crop):
-    (_, cnts) = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, cnts = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # result = cv2.drawContours(ori_crop, cnts, -1, (0, 0, 255), 2)
-    print(cnts)
+    # print(cnts)
     # return result
 
 gray0 = cov_gray(crop0)
@@ -40,20 +48,16 @@ blur0 = blur_st(gray0)
 blur1 = blur_st(gray1)
 blur2 = blur_st(gray2)
 
-edge0 = edge_detection(blur0)
-edge1 = edge_detection(blur1)
-edge2 = edge_detection(blur2)
+edge0 = edge_detection(blur0, 20, 30)
+edge1 = edge_detection(blur1, 40, 140)
+edge2 = edge_detection(blur2, 50, 120)
 
-# blur_0 = blur_st(edge0)
-# blur_1 = blur_st(edge1)
-# blur_2 = blur_st(edge2)
+# draw0 = draw_contour(edge0, crop0)
+# draw1 = draw_contour(edge1, crop1)
+# draw2 = draw_contour(edge2, crop2)
 
-draw0 = draw_contour(edge0, crop0)
-draw1 = draw_contour(edge1, crop1)
-draw2 = draw_contour(edge2, crop2)
-
-# show(draw0, "draw0")
-# show(draw1, "draw1")
-# show(draw2, "draw2")
+show(edge0, "edge0")
+show(edge1, "edge1")
+show(edge2, "edge2")
 
 cv2.waitKey(0)
