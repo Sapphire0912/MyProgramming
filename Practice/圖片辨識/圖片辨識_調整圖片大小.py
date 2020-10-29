436dbfda659c10f4355a35f0cc03b9a5547312e7
Sapphire0912 <kotori228520@gmail.com>
2020-10-29 10:33:43 +0800

    test

--- Practice/圖片辨識/圖片辨識_調整圖片大小.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-img = cv2.imread('target.jpg')
-modify = cv2.resize(img, (300, 300), interpolation = cv2.INTER_NEAREST)
-cv2.imshow("img", img)
-cv2.imshow("modify", modify)
-
-cv2.waitKey(0)
