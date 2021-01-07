20767330cb2aa9890fbea28ccbd0f748239e8c25
Sapphire0912 <57748276+Sapphire0912@users.noreply.github.com>
2021-01-07 21:50:17 +0800

    debug

--- debug_file/ide_test.py
+++ debug_file/ide_test.py
@@ -2,5 +2,10 @@ print("IDE is ok.")
 
 # module test
 import numpy
-a = numpy.arange(1, 10, 2)
-print(a)
+# a = numpy.arange(1, 10, 2)
+# print(a)
+
+from sklearn.datasets import load_iris
+iris = load_iris()
+X, y = iris.data, iris.target
+# print(X.shape, y.shape)
