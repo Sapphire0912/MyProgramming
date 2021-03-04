6adf7943fcc8b22fcfa87c8d9bcaf3f6a5106527
Sapphire0912 <57748276+Sapphire0912@users.noreply.github.com>
2021-03-05 07:39:39 +0800

    Practice

--- Python/Practice/opencv_practice/opencv_use.py
+++ Python/Practice/opencv_practice/opencv_use.py
@@ -1,13 +1,19 @@
 import cv2
 import numpy as np
 
+
+def output_img(img, text = 'My image'):
+    cv2.imshow(text, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 # read image
 path = "E:\\MyProgramming\\Python\\Practice\\opencv_practice\\fbk01.jpg"
 fbk = cv2.imread(path)
-print(fbk.shape)
+output_img(fbk)
 
+# turn into grayscale
+fbk_gray = cv2.cvtColor(fbk, cv2.COLOR_BGR2GRAY)
+output_img(fbk_gray)
 
-# output image
-cv2.imshow('my image', fbk)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
