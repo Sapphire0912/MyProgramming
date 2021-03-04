48f824a4c3fa3dbe198135e1897bea431340bf04
Sapphire0912 <57748276+Sapphire0912@users.noreply.github.com>
2021-03-05 07:52:01 +0800

    Practice

--- Python/Practice/opencv_practice/opencv_use.py
+++ Python/Practice/opencv_practice/opencv_use.py
@@ -1,8 +1,14 @@
 import cv2
+import seaborn as sns
+import matplotlib.pyplot as plt
 import numpy as np
 
+# visualize
+sns.set()
 
-def output_img(img, text = 'My image'):
+
+# 目標去背景
+def output_img(img, text='My image'):
     cv2.imshow(text, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -11,9 +17,13 @@ def output_img(img, text = 'My image'):
 # read image
 path = "E:\\MyProgramming\\Python\\Practice\\opencv_practice\\fbk01.jpg"
 fbk = cv2.imread(path)
-output_img(fbk)
+# output_img(fbk)
 
 # turn into grayscale
 fbk_gray = cv2.cvtColor(fbk, cv2.COLOR_BGR2GRAY)
-output_img(fbk_gray)
+# output_img(fbk_gray)
+
+# gaussian filter
+fbk_gaussian = cv2.GaussianBlur(fbk_gray, (3, 3), 0)
+# output_img(fbk_gaussian)
 
