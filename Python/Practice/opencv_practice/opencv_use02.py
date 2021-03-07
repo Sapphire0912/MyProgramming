60b9fd69e75d4dbba92152fde3f8b18c3592b5c4
Sapphire0912 <57748276+Sapphire0912@users.noreply.github.com>
2021-03-07 17:03:04 +0800

    Practice

--- Python/Practice/opencv_practice/opencv_use02.py
+++ Python/Practice/opencv_practice/opencv_use02.py
@@ -1,1 +1,6 @@
 import cv2
+
+# image source: https://www.pixiv.net/en/artworks/81438832 <- rushia
+# image source: https://www.pixiv.net/en/artworks/84349476 <- polka
+# image source: myself <- pet
+# the picture is for learning only
