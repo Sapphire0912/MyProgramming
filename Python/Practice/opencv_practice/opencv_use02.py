2b927d39d871158a7a3fbd170cce5419511e7532
Sapphire0912 <57748276+Sapphire0912@users.noreply.github.com>
2021-03-07 17:20:44 +0800

    Practice

--- Python/Practice/opencv_practice/opencv_use02.py
+++ Python/Practice/opencv_practice/opencv_use02.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Target: remove background and take out character or parrot
 # image source: https://www.pixiv.net/en/artworks/81438832 <- rushia
@@ -8,3 +9,7 @@ import cv2
 # all photos will be removed if copyright infringement
 # mail: kotori228520@gmail.com
 
+# read image
+path = "E:/MyProgramming/Python/Practice/opencv_practice/rushia/rushia_ori.jpg"
+rushia = cv2.imread(path)
+# print(rushia.shape)  # (1500, 1052, 3)
