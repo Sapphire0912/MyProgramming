129d120efe8e3367423cffada0c185f9eba542ad
Sapphire0912 <57748276+Sapphire0912@users.noreply.github.com>
2021-03-11 19:45:16 +0800

    Practice

--- Python/Practice/opencv_practice/image_algorithm.py
+++ Python/Practice/opencv_practice/image_algorithm.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 
 # image source: https://www.pixiv.net/en/artworks/81438832
 # the picture is for learning only
@@ -8,6 +9,7 @@ import numpy as np
 
 
 def output_img(img, text='My image'):
+    cv2.namedWindow(text, cv2.WINDOW_NORMAL)
     cv2.imshow(text, img)
     cv2.imwrite('%s.png' % text, img)
 
@@ -29,6 +31,39 @@ class MyImgAlg(object):
     def gaussian_filter(self, img):
         pass
 
+    def sobel(self, img, dx=0, dy=0):
+        gx_kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+        gy_kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+        # method 1
+        # 建立一個為0且大於原始圖片兩個pixel的陣列
+        length, width = img.shape
+        con_img = np.zeros((length+2, width+2), dtype=np.uint8)
+
+        # 將圖像存入
+        con_img[1:length+1, 1:width+1] = img
+
+        # 暫時建立空陣列存放計算後的結果
+        img_gx = np.zeros(img.shape, dtype=np.uint32)
+        img_gy = np.zeros(img.shape, dtype=np.uint32)
+        result_img = np.zeros(img.shape, dtype=np.uint8)
+
+        # 卷積(convolution)
+        # dx 1次
+        for y_axis in range(0, length-1):
+            for x_axis in range(0, width-1):
+                img_gx[y_axis][x_axis] = np.sum(con_img[y_axis:y_axis+3, x_axis:x_axis+3] * gx_kernel)
+
+        # dy 1次
+        for y_axis in range(0, length-1):
+            for x_axis in range(0, width-1):
+                img_gy[y_axis][x_axis] = np.sum(con_img[y_axis:y_axis+3, x_axis:x_axis+3] * gy_kernel)
+
+        # 計算 sqrt(Gx^2 + Gy^2)
+        result_img = np.sqrt(img_gx * img_gx + img_gy * img_gy).astype(np.uint8)
+        # print(result_img[0][0])
+        return result_img
+
 
 # read image
 path = "E:/MyProgramming/Python/Practice/opencv_practice/rushia/rushia_ori.jpg"
@@ -43,6 +78,10 @@ rushia_gaussian = cv2.GaussianBlur(rushia_gray, (3, 3), 0)
 # print(cv2.getGaussianKernel(3, 0))
 # output_img(rushia_gaussian, text='./rushia/rushia_gray_gaussian_3x3')
 
+rushia_sobel = cv2.Sobel(rushia_gray, ddepth=-1, dx=1, dy=1, ksize=3)
+# print(rushia_sobel)
+# output_img(rushia_sobel, text='./rushia/rushia_gray_sobel_dxdy_3x3')
+
 # use my image algorithm
 my_alg = MyImgAlg()
 my_rushia_gray = my_alg.conv_to_gray(rushia)
@@ -50,6 +89,13 @@ my_rushia_gray = my_alg.conv_to_gray(rushia)
 # print(my_rushia_gray.dtype)
 # condition1: the type of image data output using the cv2.imshow() is np.uint8
 # output_img(my_rushia_gray, "./rushia/rushia_my_alg_gray")
+
+start_time = time.time()
+my_rushia_sobel = my_alg.sobel(my_rushia_gray, dx=1, dy=1)
+end_time = time.time()
+output_img(my_rushia_sobel, text='./rushia/rushia_my_alg_sobel')
+print("cost time: ", end_time - start_time)
+# cv2.imshow("my_sobel", my_rushia_sobel)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
