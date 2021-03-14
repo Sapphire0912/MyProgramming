812521786b4091d50067105d0c835eb4d3aaf0b6
Sapphire0912 <57748276+Sapphire0912@users.noreply.github.com>
2021-03-15 07:34:43 +0800

    Practice

--- Python/Practice/opencv_practice/opencv_lbp.py
+++ Python/Practice/opencv_practice/opencv_lbp.py
@@ -1,8 +1,18 @@
+from skimage.feature import local_binary_pattern
+import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import time
 
 
 # Target: Using LBP algorithm and take out the road
+# LBP algorithm
+# 1. 原始圖片轉灰階且設定 cells 大小 OK
+# 2. 將灰階圖片依照 cells 大小切割後, 切割後的正中間為中心點當成 threshold OK
+# 3. 選擇鄰近區域要依順時針或逆時針旋轉, 若值大於 threshold 給 1 否則 0 (1 if region > threshold else 0) OK
+# 4. 接著按照旋轉方向取 2進制 轉成 10進制 後的值 OK
+# 5. 繪製直方圖 ???
+
 def output_img(img, text):
     cv2.namedWindow(text, cv2.WINDOW_NORMAL)
     cv2.imshow(text, img)
@@ -40,8 +50,36 @@ road_gray = cv2.cvtColor(road, cv2.COLOR_BGR2GRAY)
 # print(road_gray[0:6, 0:6])
 # output_img(road_gray, text='./road/road_gray')
 
-my_lbp(road_gray)
+# use my lbp
+# st = time.time()
+# my_lbp(road_gray)
+# end = time.time()
+# print("cost time: ", end - st)  # cost time:  2.0037386417388916 s
 
+# ----------
+# use scikit-image module lbp
+# use matplotlib imshow lbp image
+radius = 1
+n_points = 8 * radius
+
+# lbp method returns the dtype and value of the image(current only the image)
+# default: dtype float64, value 0 to 255
+# ror: dtype float64, value 0 to 255
+# nri_uniform: dtype float64, value 0 to 58
+# uniform: dtype float64, value 0 to 9
+# var: dtype float64, value has han
+
+lbp = local_binary_pattern(road_gray, n_points, radius, method='var')
+# print(lbp[30:40, 790:800])
+# print(np.max(lbp), np.min(lbp))
+# plt.imshow(lbp, cmap='gray')
+# plt.show()
+
+# output_img(lbp, text="./road/road_lbp_var")
+
+# image 無法儲存
+# edges = filters.sobel(road_gray)
+# output_img(edges, text="./road/road_skimage_sobel")
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
